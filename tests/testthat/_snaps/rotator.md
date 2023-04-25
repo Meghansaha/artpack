393ca6c18efbe69212d1e2d5b3a710416fbe48c0
meghansaha <meghanha01@gmail.com>
2023-04-24 21:06:57 -0400

    Adding test coverage to rotator

---
+++ tests/testthat/_snaps/rotator.md
@@ -0,0 +1,4 @@
+# Test to ensure that error is thrown when dataframe is not input
+
+    x `data` should be of type data frame or a tibble, not <integer>
+
--- tests/testthat/test-rotator.R
+++ tests/testthat/test-rotator.R
@@ -5,15 +5,17 @@ rotated_square <- rotator(data = original_square,
                           angle = 45,
                           anchor = "center")
 
+
+
 # Test to check if functions changes data frame inputted
 test_that("Test for change to the original dataframe", {
   expect_false(identical(original_square,rotated_square))
-}
-
-)
+})
 
 # Test to check that error occurs if dataframe is not input
-test_that("Test to ensure that error is thrown when dataframe is not input",{
-  expect_error(rotator(nums))
-}
-          )
+test_that("Test to ensure that error is thrown when dataframe is not input", {
+  nums <- 1:5
+  expect_snapshot_error(rotator(nums))
+})
+
+
