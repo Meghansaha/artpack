e6cdb6f754c6998894e092a9cb240d521235837e
meghansaha <meghanha01@gmail.com>
2023-08-21 10:33:31 -0400

    created helper function `group_numbers()`

---
+++ R/group_numbers.R
@@ -0,0 +1,70 @@
+#' Convert Numbers into Padded Strings for Easier Group Numbering
+#'
+#' @param ... #A numeric vector with a length of at least 1.
+#'
+#' @return #A Character Vector
+#'
+#' @importFrom purrr map
+#' @importFrom purrr list_c
+#' @importFrom stringr str_pad
+#' @importFrom stringr str_length
+#' @importFrom cli cli_abort
+#'
+#' @export
+#'
+#' @examples
+#' # Useful for easier group numbering so groups are ordered as intended
+#' # Expects a numeric vector of numbers to convert to padded numbers
+#' regular_numbers <- 1:19
+#' padded_numbers <- group_numbers(regular_numbers)
+#'
+#' # The padding matters when creating labels for groupings
+#' # as numbers will be converted to characters if attached to strings.
+#' # Sorts as expected:
+#' sort(regular_numbers)
+#'
+#' # Does not as a character:
+#' sort(paste0("group_", regular_numbers))
+#'
+#' # Will sort as expected when padded:
+#' sort(paste0("group_", padded_numbers))
+
+group_numbers <- function(...){
+
+  #===========================================================================#
+  # Input Check----------------------------------------------------------------
+  #===========================================================================#
+
+  if (!is.numeric(...)) {
+    c(
+      paste("The input for `group_numbers()` must be of class", callout("<numeric>")),
+      "x" = paste("The input you've supplied: {.var {...}} is of class", error("{.cls{class(...)}}")),
+      "i" = "Check the `group_numbers()` input."
+    ) |>
+      cli::cli_abort()
+
+    }
+
+    #=========================================================================#
+    # Numbering Conversion-----------------------------------------------------
+    #=========================================================================#
+
+    # Grab width of largest number#
+    string_width <- stringr::str_length(max(...))
+
+    # Sort the input
+    sorted <- sort(c(...))
+
+    # Convert to padded strings
+    out_numbers <-
+      purrr::map(sorted, ~stringr::str_pad(.x,
+                                           width = string_width,
+                                           side = "left",
+                                           pad = "0"
+                                           )
+                 ) |>
+      purrr::list_c()
+
+    return(out_numbers)
+
+  }
